ab21e293d46967f44fb160172c67fe0db9ac9e27
Kaili Gregory <kaili.gregory2012@gmail.com>
2026-03-18 16:00:41 -0400

    change plot color

--- BUMBLEBEE.R
+++ BUMBLEBEE.R
@@ -1,8 +1,8 @@
 tuna <- read.csv("https://raw.githubusercontent.com/henryjhershey/FORK-ME-CRU/refs/heads/main/TunaData.csv?",header=T)
 
 summary(tuna)
-pdf("YOUR NAME.pdf")
-plot(tuna$catch~tuna$year, type="l",lwd=4 
+pdf("KAILI.pdf")
+plot(tuna$catch~tuna$year, type="l",lwd=4, col="red"
      #,color = CHANGE)
 )
 lines(tuna$index ~ tuna$year #,lty=CHANGE)
